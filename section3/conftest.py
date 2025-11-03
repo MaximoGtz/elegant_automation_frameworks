3b83a06c1a10b935a1e231eacc381378b76826ce
Maximo Gutierrez <maximogtzog@gmail.com>
2025-11-03 14:19:45 -0600

    last commit of the day

---
+++ section3/conftest.py
@@ -0,0 +1,18 @@
+from pytest import fixture
+from selenium import webdriver
+
+@fixture(scope="session")
+def chrome_browser():
+    browser = webdriver.Chrome()
+    yield browser
+    #teardown
+    #code that executes before the yield returns some data
+    print("I am tearing down this browser")
+
+@fixture(scope="function")
+def get_configuration():
+    config = [
+        3006,
+        "database",
+        "mysql"
+    ]
