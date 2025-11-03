04d58c97c985ceaf1c1034ed3e4b89c0b9bd1ac8
Maximo Gutierrez <maximogtzog@gmail.com>
2025-11-03 11:10:24 -0600

    Created a new mark and tried a couple of things
    t
    
    exit
    close
    git push
    
    B

--- section3/tests/sportscar/body/test_body.py
+++ section3/tests/sportscar/body/test_body.py
@@ -2,3 +2,8 @@ from pytest import mark
 @mark.body
 def test_body_functions_as_expected():
     assert True
+
+@mark.body
+@mark.regression
+def test_body_functions_not_expected():
+    assert False == False
