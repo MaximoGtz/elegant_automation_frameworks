23b095f424ee07d1403cad1c8e56334f11eb84d0
Maximo Gutierrez <maximogtzog@gmail.com>
2025-11-03 14:20:48 -0600

    to put more changes

--- section3/tests/sportscar/body/test_body.py
+++ section3/tests/sportscar/body/test_body.py
@@ -2,7 +2,7 @@ from pytest import mark
 @mark.body
 def test_body_functions_as_expected():
     assert True
-
+# just a comment
 @mark.body
 @mark.regression
 def test_body_functions_not_expected():
